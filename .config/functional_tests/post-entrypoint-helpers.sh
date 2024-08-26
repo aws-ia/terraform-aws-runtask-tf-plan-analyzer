fa15e745e1934c6e54e6154633b901528ef7a117
wellsiau-aws <wellsiau@amazon.com>
2024-08-25 17:22:32 -0700

    functional-test

--- .config/functional_tests/post-entrypoint-helpers.sh
+++ .config/functional_tests/post-entrypoint-helpers.sh
@@ -4,3 +4,13 @@
 ## TIPS: avoid modifying the .project_automation/functional_test/entrypoint.sh
 ## migrate any customization you did on entrypoint.sh to this helper script
 echo "Executing Post-Entrypoint Helpers"
+
+#********** Project Path *************
+PROJECT_PATH=${BASE_PATH}/project
+PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+cd ${PROJECT_PATH}
+
+#********** CLEANUP *************
+echo "Cleaning up all temp files and artifacts"
+cd ${PROJECT_PATH}
+make -s clean
--- .config/functional_tests/pre-entrypoint-helpers.sh
+++ .config/functional_tests/pre-entrypoint-helpers.sh
@@ -4,3 +4,18 @@
 ## TIPS: avoid modifying the .project_automation/functional_test/entrypoint.sh
 ## migrate any customization you did on entrypoint.sh to this helper script
 echo "Executing Pre-Entrypoint Helpers"
+
+
+#********** Project Path *************
+PROJECT_PATH=${BASE_PATH}/project
+PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+cd ${PROJECT_PATH}
+
+#********** TFC Env Vars *************
+export AWS_DEFAULT_REGION=us-west-2
+export TFE_TOKEN=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
+export TF_TOKEN_app_terraform_io=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
+
+#********** MAKEFILE *************
+echo "Build the lambda function packages"
+make all
